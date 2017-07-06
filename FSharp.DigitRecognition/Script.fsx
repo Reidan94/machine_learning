a63daa259499985b71a69c7509755b9ea3354ecb
Reidan94 <reidanabdi94@gmal.com>
2017-07-06 23:00:34 +0300

    Fsharp digit recognition. Primitive implementation.

---
+++ FSharp.DigitRecognition/Script.fsx
@@ -0,0 +1,45 @@
+﻿open System.IO
+open System
+
+type Observation = {Label:string; Pixels:double[]}
+type Distance = double[] * double[] -> double
+
+let toObservation(csvData:string) = 
+    let splitted = csvData.Split([|","|], StringSplitOptions.RemoveEmptyEntries)
+    let label = splitted.[0]
+    let pixels = splitted.[1..] |> Array.map double
+    {Label = label; Pixels = pixels }
+
+let reader path = 
+    let data = File.ReadAllLines path
+    data.[1..]|> Array.map toObservation
+
+let manhattanDistance(pixels1, pixels2) = 
+    Array.zip pixels1 pixels2
+    |> Array.map (fun(x, y) -> abs(x - y))
+    |> Array.sum
+
+let euclideanDistance(pixels1, pixels2) = 
+    Array.zip pixels1 pixels2
+    |> Array.map (fun(x, y) -> pown (x - y) 2)
+    |> Array.sum
+    |> sqrt
+
+let train (trainingData:Observation[]) (dist:Distance) = 
+    let classify(pixels:double[]) =
+        trainingData
+        |> Array.minBy (fun x -> dist(x.Pixels, pixels))
+        |> fun best -> best.Label
+    classify
+
+// Chose correct path
+let trainingPath = @"E:\OpenSource\Repository\Machine Learning 2\trainingsample.csv"
+let trainingData = reader trainingPath  
+let classifierManhattan  = train trainingData manhattanDistance
+let classifierEuclidean = train trainingData euclideanDistance
+
+let validationPath = @"E:\OpenSource\Repository\Machine Learning 2\validationsample.csv"
+let validationData = reader validationPath
+validationData
+|> Array.averageBy (fun x-> if classifierEuclidean x.Pixels = x.Label then 100. else 0.)
+|> printfn "Correct: %.3f"
